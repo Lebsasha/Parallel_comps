c75856af2be0061a23974f88b1d85565f5225c68
Alexander <lebedevskiy_s@rambler.ru>
2020-11-17 10:43:10 +0300

    Indent

--- Temp_mpi/main.cpp
+++ Temp_mpi/main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <cstring>
+
 #ifdef CBB
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #include </usr/include/x86_64-linux-gnu/mpich/mpi.h>
 #pragma GCC diagnostic pop
 #else // CBB
+
 #include <mpich/mpi.h>
+
 #endif // CBB
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    int * p = new int;
+    int* p = new int;
     std::string message;
     MPI_Status status = MPI_Status();
-	 MPI_Init(&argc, &argv);
-	 MPI_Comm_size(MPI_COMM_WORLD, p);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, p);
     int myrank = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank );
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     if (myrank == 0)
-    MPI_Send("Hello", strlen("Hello"), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Send("Hello", strlen("Hello"), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
     else
     {
         MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-        std::cout<<"Hello"<<std::endl;
+        std::cout << "Hello" << std::endl;
     }
-	MPI_Finalize();
+    MPI_Finalize();
 }
