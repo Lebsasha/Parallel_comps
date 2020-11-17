097eec97a525705cb22bbf5be5eb387d5fad0a06
Alexander <lebedevskiy_s@rambler.ru>
2020-11-17 09:17:19 +0300

    First function

--- Temp_mpi/main.cpp
+++ Temp_mpi/main.cpp
@@ -2,9 +2,11 @@
 #include <mpich/mpi.h>
 int main(int argc, char **argv)
 {
+    int * p = new int;
 	 MPI_Init(&argc, &argv);
+	 MPI_Comm_size(MPI_COMM_WORLD, p);
   {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n"<<*p;
   }
 	MPI_Finalize();
 }
