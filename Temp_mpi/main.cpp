c3f7ddcf2ba0e5fa13d4be79582d5bcd9b827a22
Alexander <lebedevskiy_s@rambler.ru>
2020-11-17 10:42:41 +0300

    First program (not runnable)

--- Temp_mpi/main.cpp
+++ Temp_mpi/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #ifdef CBB
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #pragma GCC diagnostic ignored "-Wcast-qual"
@@ -11,10 +12,18 @@
 int main(int argc, char **argv)
 {
     int * p = new int;
+    std::string message;
+    MPI_Status status = MPI_Status();
 	 MPI_Init(&argc, &argv);
 	 MPI_Comm_size(MPI_COMM_WORLD, p);
-  {
-    std::cout << "Hello World!\n"<<*p;
-  }
+    int myrank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank );
+    if (myrank == 0)
+    MPI_Send("Hello", strlen("Hello"), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+    else
+    {
+        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        std::cout<<"Hello"<<std::endl;
+    }
 	MPI_Finalize();
 }
