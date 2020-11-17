37b980db4792fd30069ea6a1217dae7ca1d29a2b
Alexander <lebedevskiy_s@rambler.ru>
2020-11-17 10:05:34 +0300

    Added lib to CB

--- Temp_mpi/main.cpp
+++ Temp_mpi/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#ifdef CBB
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#include </usr/include/x86_64-linux-gnu/mpich/mpi.h>
+#pragma GCC diagnostic pop
+#else // CBB
 #include <mpich/mpi.h>
+#endif // CBB
+
 int main(int argc, char **argv)
 {
     int * p = new int;
