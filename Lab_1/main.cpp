686cabdf9f5b1fdfa0aa564daa68e1e730f6df15
Alexander <lebedevskiy_s@rambler.ru>
2020-11-24 12:13:10 +0300

    Upgraded Lab_1

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -14,79 +14,25 @@
 
 #endif // CBB
 
-//1 2 3
-//4 5 6
-//7 8 9
 int main(int argc, char** argv)
 {
     std::string message="Info";
-    MPI_Status status = MPI_Status();
     MPI_Init(&argc, &argv);
+    MPI_Status status = MPI_Status();
     int p;
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-    int n = sqrt(p);
+    int n = static_cast<int>(sqrt(p));
     assert(n*n==p);
     int myrank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    if (myrank==0)//1
-    {
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+1, 0, MPI_COMM_WORLD);
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+n, 0, MPI_COMM_WORLD);
-    }
-    else if(myrank+1==n)//3
-    {
+    if(myrank%n!=0)
         MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-1, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+n, 0, MPI_COMM_WORLD);
-    }
-    else if(myrank==n*(n-1))//7
-    {
+    if(myrank>n-1)
         MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-n, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
+    std::cout<<message<<myrank<<std::endl;
+    if((myrank+1)%n!=0)
         MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+1, 0, MPI_COMM_WORLD);
-    }
-    else if(myrank==n*n-1)//9
-    {
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-1, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-n, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-    }
-    else if(myrank > 0 && myrank <n-1)//2
-    {
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-1, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+1, 0, MPI_COMM_WORLD);
+    if(myrank<n*(n-1))
         MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+n, 0, MPI_COMM_WORLD);
-    }
-    else if((myrank)%n == 0)//4
-    {
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-n, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+1, 0, MPI_COMM_WORLD);
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+n, 0, MPI_COMM_WORLD);
-    }
-    else if ((myrank+1)%n == 0)//6
-    {
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-1, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-n, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+n, 0, MPI_COMM_WORLD);
-    }
-    else if (myrank - n*(n-1)>=0&&myrank - n*(n-1)<n-1)//8
-    {
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-1, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-n, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+1, 0, MPI_COMM_WORLD);
-    }
-    else //5
-    {
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-1, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, myrank-n, 0, MPI_COMM_WORLD, &status);
-        std::cout<<message<<myrank<<std::endl;
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+1, 0, MPI_COMM_WORLD);
-        MPI_Send(message.c_str(), message.length(), MPI_CHAR, myrank+n, 0, MPI_COMM_WORLD);
-    }
     MPI_Finalize();
 }
