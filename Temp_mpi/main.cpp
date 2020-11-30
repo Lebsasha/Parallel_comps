025bb5d6ea9e2226423e7a8b8520caa8dd1d1b51
Alexander <lebedevskiy_s@rambler.ru>
2020-11-30 23:51:48 +0300

    Replace "" string with std::string

--- Temp_mpi/main.cpp
+++ Temp_mpi/main.cpp
@@ -15,18 +15,18 @@
 int main(int argc, char** argv)
 {
     int* p = new int;
-    std::string message;
+    std::string message="Hello_world";
     MPI_Status status = MPI_Status();
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, p);
     int myrank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     if (myrank == 0)
-        MPI_Send("Hello", strlen("Hello"), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(message.c_str(), message.length(), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
     else
     {
         MPI_Recv((void*) message.c_str(), 20, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-        std::cout << "Hello" << std::endl;
+        std::cout <<message << std::endl;
     }
     MPI_Finalize();
 }
