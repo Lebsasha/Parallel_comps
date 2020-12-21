609bf6f69ce59eb69e3ed5c24ae22820b534acf2
Alexander <lebedevskiy_s@rambler.ru>
2020-12-22 02:21:29 +0300

    Third Lab_4 ref

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <cstring>
-#include <cassert>
 #include <iomanip>
-
 #include <omp.h>
 
 using namespace std;
@@ -14,64 +11,44 @@ template<typename T>
 void View(T* pMatrix, size_t n_row, size_t n_col);
 
 
-///root process
-int q;
-
 int main(int argc, char** argv)
 {
     omp_set_num_threads(4);
-    int p = 1;
-    const size_t n = 4;
-    q = 0;
-    assert(q < p);
-    int* A, * B, * C, * D, * buffer;
-    int* A_row = new int[n];
-    int* B_col = new int[n];
-    int* C_col = new int[n];
-    bool signal_i = true;
-    bool signal_j = true;
-    int last_active_process;
+    const size_t n = 1000;
+    int* A, * B, * C, * D;
     double computation_time;
-
     A = new int[n * n];
     B = new int[n * n];
     C = new int[n * n];
     D = new int[n * n];
-    for (size_t i = 0; i < n * n; ++i)
+    for (int i = 0; i < (int) (n * n); ++i)
     {
         A[i] = i + 1;
         B[i] = i + 1;
         C[i] = i + 1;
         D[i] = 0;
     }
-    buffer = new int[n];
-    last_active_process = p - 1;
     computation_time = omp_get_wtime();
-// TODO parallel by j
 
-#pragma omp parallel sections default(none) shared(A, B, C, D, cout)
+#pragma omp parallel sections default(none) shared(A, B, C, D)
     {
 #pragma omp section
         {
-            for (size_t i = 0; i < n/2; ++i)
-            {
+            for (size_t i = 0; i < n / 2; ++i)
                 for (size_t j = 0; j < n; ++j)
                     for (size_t k = 0; k < n; ++k)
                     {
                         D[i * n + j] += A[i * n + k] * (B[k * n + j] + C[k * n + j]);
                     }
-            }
         }
 #pragma omp section
         {
-            for (size_t i = n/2; i < n; ++i)
-            {
+            for (size_t i = n / 2; i < n; ++i)
                 for (size_t j = 0; j < n; ++j)
                     for (size_t k = 0; k < n; ++k)
                     {
                         D[i * n + j] += A[i * n + k] * (B[k * n + j] + C[k * n + j]);
                     }
-            }
         }
     }
 
@@ -82,18 +59,6 @@ int main(int argc, char** argv)
     delete[] B;
     delete[] C;
     delete[] D;
-
-//    180 200 220 240
-//    404 456 508 560
-//    628 712 796 880
-//    852 968 1084 1200
-
-
-    delete[] buffer;
-    delete[] A_row;
-    delete[] B_col;
-    delete[] C_col;
-
     return 0;
 }
 
